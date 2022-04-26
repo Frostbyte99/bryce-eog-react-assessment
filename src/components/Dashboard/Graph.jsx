5645b7a15293ec69c374955285d6abef47dd46cd
Bryce Colby <frostbyte498@gmail.com>
2022-04-25 23:49:13 -0500

    changed graph to render only one graph

--- src/components/Dashboard/Graph.jsx
+++ src/components/Dashboard/Graph.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { gql, useQuery } from '@apollo/client';
-import { Typography, Chip } from '@material-ui/core';
+/* eslint-disable no-plusplus */
+/* eslint-disable no-bitwise */
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
 import {
   LineChart,
   XAxis,
@@ -9,63 +10,53 @@ import {
   Tooltip,
   Legend,
   Line,
+  ResponsiveContainer,
 } from 'recharts';
+import moment from 'moment';
+import stc from 'string-to-color';
 
-const query = gql`
-  query lastKnown($metric: String!) {
-    getLastKnownMeasurement(metricName: $metric) {
-      value
-      unit
-      at
-    }
-  }
-`;
-
-export default ({ metricProp }) => {
-  const [metrics, setMetrics] = useState([]);
-  const { data, error, loading } = useQuery(query, {
-    variables: { metric: metricProp },
-    fetchPolicy: 'network-only',
-    pollInterval: 1300,
-    onCompleted: () => {
-      const date = new Date(data.getLastKnownMeasurement.at);
-      const hour = date.getHours();
-      const minutes = date.getMinutes();
-      data.getLastKnownMeasurement.at = `${hour}:${minutes}`;
-      setMetrics([...metrics, data.getLastKnownMeasurement]);
-    },
-  });
-
-  if (loading) return <Typography> loading... </Typography>;
-  if (error) return <Typography color="error">{error}</Typography>;
-  if (!data) return <Chip label="Metrics not found" />;
-
-  return (
-    <>
-      <LineChart
-        width={700}
-        height={300}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="at" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line
-          data={metrics}
-          label={metricProp}
-          type="monotone"
-          dataKey="value"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
-    </>
-  );
-};
+export default ({ metricData }) => (
+  <ResponsiveContainer width="80%" height="70%">
+    <LineChart
+      width={700}
+      height={300}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <Tooltip labelFormatter={(value) => `Time: ${moment(value).format('hh:mm:ss A')}`} />
+      <Legend />
+      <XAxis
+        dataKey="at"
+        domain={['auto', 'auto']}
+        name="Time"
+        tickFormatter={(unixTime) => moment(unixTime).format('HH:mm')}
+        type="number"
+      />
+      <YAxis
+        yAxisId="PSI"
+        dataKey="value"
+        label={{ value: 'PSI', position: 'insideTopLeft', dy: -10 }}
+      />
+      <YAxis yAxisId="F" dataKey="value" label={{ value: 'F', position: 'insideTopLeft', dy: -10 }} />
+      <YAxis yAxisId="%" dataKey="value" label={{ value: '%', position: 'insideTopLeft', dy: -10 }} min={0} max={100} />
+      {metricData.map((metric, index) => (
+        <>
+          <Line
+            key={index}
+            data={metric.measurements}
+            type="monotone"
+            dataKey="value"
+            name={metric.measurements[0].metric}
+            stroke={stc(metric.measurements[0].metric)}
+            yAxisId={metric.measurements[0].unit}
+          />
+        </>
+      ))}
+    </LineChart>
+  </ResponsiveContainer>
+);
