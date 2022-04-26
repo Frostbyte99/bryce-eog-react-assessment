d1d346c4297d226201c841d29fb9c6b5787e3e10
Bryce Colby <frostbyte498@gmail.com>
2022-04-25 21:55:17 -0500

    added graphData to poll historical data

---
+++ src/components/Dashboard/GraphData.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable array-callback-return */
+import React, { useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import Graph from './Graph';
+
+const query = gql`
+  query getMultiple($inputs: [MeasurementQuery]!) {
+    getMultipleMeasurements(
+      input: $inputs
+    ) {
+      measurements {
+        metric
+        value
+        at
+        unit
+      }
+    }
+  }
+`;
+
+export default ({ metricSelection }) => {
+  if (metricSelection === undefined) {
+    console.log('undefined');
+    console.log(metricSelection);
+    return <></>;
+  }
+
+  const [metrics, setMetrics] = useState([]);
+  const [queryArgs, setQueryArgs] = useState([]);
+
+  useEffect(() => {
+    const historical = new Date() - 30 * 60 * 1000;
+    metricSelection.map((metric) => {
+      setQueryArgs([
+        ...queryArgs,
+        { metricName: metric, after: historical },
+      ]);
+    });
+  }, [metricSelection]);
+
+  const { data } = useQuery(query, {
+    variables: { inputs: queryArgs },
+    pollInterval: 1300,
+    onCompleted: () => {
+      setMetrics(data.getMultipleMeasurements);
+    },
+  });
+
+  return <Graph metricData={metrics} />;
+};
